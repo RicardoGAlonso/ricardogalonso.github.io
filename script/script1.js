23f6ebee2c0718dffb61e1659541bad3641f44c9
Ricardo Alonso <ricardogalonso@hotmail.com>
2024-01-22 15:13:14 -0500

    Ok

--- script/script1.js
+++ script/script1.js
@@ -1,23 +1,23 @@
 "use strict";
 
-alert("My alert 01");
+// alert("My alert 01");
 
-let isBoss = confirm("Are you the boss?");
+// let isBoss = confirm("Are you the boss?");
 
-alert(isBoss); // true if OK is pressed
+// alert(isBoss); // true if OK is pressed
 
-let condition = true;
+// let condition = true;
 
-function showMessage() {
-  alert("Hello everyone!");
-}
+// function showMessage() {
+//   alert("Hello everyone!");
+// }
 
-if (condition) {
-  if (condition) {
-    if (condition) {
-    } else {
-    }
-  } else {
-  }
-} else {
-}
+// if (condition) {
+//   if (condition) {
+//     if (condition) {
+//     } else {
+//     }
+//   } else {
+//   }
+// } else {
+// }
